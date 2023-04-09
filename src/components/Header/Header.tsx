dc2983ddcd0fedefa0064acc9025ae0273d52905
Martin Maffei <martinmaffei95@gmail.com>
2023-04-09 13:17:34 -0300

    basic header is ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
 import { GiGuitarBassHead } from "react-icons/gi";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { StyledHeader } from "@/styled-components";
 const Header = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
-    <header className="flex justify-between w-full h-20 bg-neutral-700 text-neutral-50 px-4 py-2">
-      <div className="bg-red-500 h-full w-16 rounded-full flex justify-center items-center ">
-        <GiGuitarBassHead className="text-5xl" />
+    <StyledHeader>
+      <div className="header_content flex justify-between w-full max-w-screen h-20 bg-neutral-700 text-neutral-50 px-4 py-2 ">
+        <div className="bg-red-500 h-full w-16 rounded-full flex justify-center items-center ">
+          <GiGuitarBassHead className="text-5xl" />
+        </div>
+        <div
+          onClick={handleOpen}
+          className="flex justify-center items-center gap-2 text-xl uppercase"
+        >
+          <p>Menu</p>
+          {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
       </div>
-      <nav className="self-end">
+      <nav
+        className={`header_nav bg-neutral-600 bg-opacity-70  ${
+          open ? "open" : ""
+        }`}
+      >
         <ul className="flex item-self-end gap-4">
-          <li>Home</li>
-          <li>Sobre nosotros</li>
-          <li>Contacto</li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">Sobre nosotros</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contacto</Link>
+          </li>
         </ul>
       </nav>
-    </header>
+    </StyledHeader>
   );
 };
 
--- src/styled-components/Header.style.ts
+++ src/styled-components/Header.style.ts
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled("header")`
-  height: 4rem;
-  width: 100vw;
-  display: flex;
-  justify-content: space-between;
-  padding-inline: 1rem;
-  & {
-    @apply bg-neutral-100;
+  position: relative;
+  .header_content {
+    position: relative;
+    z-index: 600;
+  }
+  .header_nav {
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    backdrop-filter: blur(20px);
+    width: 100%;
+    margin: 0;
+    z-index: 100;
+    ul {
+      width: 100%;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  .open {
+    transform: translateY(100%);
   }
 `;
